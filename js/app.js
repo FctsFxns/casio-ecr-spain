89c5540e4219fc39cb2a2085a0b085f01d89b8e2
Samuel Alonso <samuel.alonso@season.es>
2015-03-19 16:25:13 +0100

    add cookies message

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
+$(document).ready(function(){
+  $.cookieCuttr({
+    cookieNotificationLocationBottom: true,
+    cookieAnalytics: true,
+    cookieDeclineButton: false,
+    cookieAnalyticsMessage: "Este sitio usa cookies. Para continuar sin cambiar su configuración, acepta el uso de cookies.",
+    cookieAcceptButtonText: "x",
+    cookieWhatAreLinkText: "Más información.",
+    cookieWhatAreTheyLink: 'conformidad-con-cookies.html',
+    cookieOverlayEnabled: false
+});
+
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
