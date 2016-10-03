041f5bae4e505bdf08307aee34ebcab35e81ae03
dcanetma <dcanetma@gmail.com>
2016-10-03 12:27:27 +0200

    landings per a hostelco

---
+++ hostelco-thanks.php
@@ -0,0 +1,23 @@
+<?php require_once('shared/_header.php'); ?>
+
+<section class="ecr-section">
+  <div class="v-align">
+    <div class="row">
+      <div class="large-6 medium-10 columns small-centered ">
+        <br><br><br><br><br><br><br>
+          <h2>Consulta tu correo electrónico</h2>
+          <p>Te hemos enviado un email que te permitirá <strong style="color:#cdcdcd;">confirmar tu asistencia</strong> a la feria HOSTELCO en Barcelona.</p>
+          <p style="line-height:1.125em"><small>Para cualquier aclaración contacta con nuestro departamento comercial llamando al teléfono <strong style="color:#fff; font-size:1.2em; white-space:nowrap">934 858 400</strong> o por email a <a href="mailto:info@casio-ecr.es" class="track" data-category="goal" data-action="contact" data-label="email" data-value="10">info@casio-ecr.es</a></small>.</p>
+          <br/>
+          <a href="http://www.hostelco.com/" class="button">Más información sobre HOSTELCO</a>
+          <br/>
+          <br/>
+          <br/>
+      </div>
+      <div class="large-7 medium-6 columns">
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php require_once('shared/_footer.php'); ?>
