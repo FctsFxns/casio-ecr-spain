fdc1f8fa71691f2c25cf81cd726a714d67055734
dcanetma <dcanetma@gmail.com>
2017-01-23 13:11:38 +0100

    update text on horeca baleares confirmation pages

--- horeca-baleares-thanks.php
+++ horeca-baleares-thanks.php
@@ -6,10 +6,10 @@
       <div class="large-6 medium-10 columns small-centered ">
         <br><br><br><br><br><br><br>
           <h2>Consulta tu correo electrónico</h2>
-          <p>Te hemos enviado un email que te permitirá <strong style="color:#cdcdcd;">confirmar tu asistencia</strong> a la feria HOSTELCO en Barcelona.</p>
+          <p>Te hemos enviado un email que te permitirá <strong style="color:#cdcdcd;">confirmar tu asistencia</strong> a la feria <strong style="color:#cdcdcd;">HORECA Baleares</strong> el mes de febrero en Palma.</p>
           <p style="line-height:1.125em"><small>Para cualquier aclaración contacta con nuestro departamento comercial llamando al teléfono <strong style="color:#fff; font-size:1.2em; white-space:nowrap">934 858 400</strong> o por email a <a href="mailto:info@casio-ecr.es" class="track" data-category="goal" data-action="contact" data-label="email" data-value="10">info@casio-ecr.es</a></small>.</p>
           <br/>
-          <a href="http://www.hostelco.com/" class="button">Más información sobre HOSTELCO</a>
+          <a href="http://horecabaleares.com/" class="button">Más información sobre HORECA Baleares</a>
           <br/>
           <br/>
           <br/>
