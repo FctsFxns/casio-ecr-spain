5f5baa6ac8314438d77c12d65a44b55ef9f59c8e
dcanetma <dcanetma@gmail.com>
2017-01-23 13:03:10 +0100

    added workflow for horeca baleares campaign

---
+++ horeca-baleares-confirmado.php
@@ -0,0 +1,24 @@
+<?php require_once('shared/_header.php'); ?>
+
+<section class="ecr-section">
+  <div class="v-align">
+    <div class="row">
+      <div class="large-6 medium-10 columns small-centered ">
+        <br><br><br><br><br><br><br>
+          <h2>¡Gracias por acompañarnos!</h2>
+          <p>Tu solicitud de asistencia a <strong style="color:#cdcdcd;">HOSTELCO</strong> ha sido confirmada.</p>
+          <p>Acabas de conseguir acceso exclusivo al mayor evento estatal en equipamiento para restauración y hostelería que se celebra del 21 al 23 de octubre en Barcelona.</p>    
+          <p>Contactaremos contigo para hacerte llegar tus entradas lo antes posible.</p>      
+          <p style="line-height:1.125em"><small>Para cualquier aclaración contacta con nuestro departamento comercial llamando al teléfono <strong style="color:#fff; font-size:1.2em; white-space:nowrap">934 858 400</strong> o por email a <a href="mailto:info@casio-ecr.es" class="track" data-category="goal" data-action="contact" data-label="email" data-value="10">info@casio-ecr.es</a></small>.</p>
+          
+          <br/>
+          <br/>
+          <br/>
+      </div>
+      <div class="large-7 medium-6 columns">
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php require_once('shared/_footer.php'); ?>
