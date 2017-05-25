ad4b229b25b2fbb9df0a0ad2338a4871e8e1a767
dcanetma <dcanetma@gmail.com>
2017-05-25 15:55:14 +0200

    added 404 page

---
+++ alpha/_assets/js/404.ie9.js
@@ -0,0 +1,3 @@
+//= require One-Pages/assets/plugins/respond
+//= require One-Pages/assets/plugins/html5shiv
+//= require general/plugins/placeholder-IE-fixes
---
+++ alpha/_assets/js/404.js
@@ -0,0 +1,3 @@
+//= require jquery/dist/jquery
+//= require jquery.migrate/jquery-migrate-1.2.1.min
+//= require bootstrap/dist/js/bootstrap.min
