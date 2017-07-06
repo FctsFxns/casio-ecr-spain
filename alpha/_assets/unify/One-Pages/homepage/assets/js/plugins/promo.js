b175b83490a17e66b7f250b242bb26596e963c1d
dcanetma <dcanetma@gmail.com>
2017-07-06 14:39:38 +0200

    new creativities for vr200 and comandero on the homepage

--- alpha/_assets/unify/One-Pages/homepage/assets/js/plugins/promo.js
+++ alpha/_assets/unify/One-Pages/homepage/assets/js/plugins/promo.js
@@ -1,7 +1,9 @@
 $(function() {
-  $(".promo-section").slick({
+	var slider = $(".promo-section");
+  slider.slick({
 		autoplay: true,
 	  autoplaySpeed: 4000,
+	  speed: 1000,
 		arrows: false,
 		dots: true,
 		pauseOnDotsHover: true,
@@ -11,6 +13,25 @@ $(function() {
 			return '<span>'+title+'</span>';
 		},
   });
+
+  // It's fine, but 'pauseOnDotsHover' and 'pauseOnHover' stops working, then it doesn't works for us. 
+	// var durationList = $('.promo-item', slider).map(function(index, item) {
+	//   return item.getAttribute('data-time');
+	// });
+
+	// var slideIndex = 0;
+	// var changeSlide = function(timing) {
+	//   setTimeout(function() {
+	//     if (timing !== 0) {
+	//       slider.slick('slickNext');
+	//     }
+	//     if (slideIndex >= durationList.length) slideIndex = 0;
+	//     changeSlide(durationList[slideIndex++]);
+
+	//   }, timing);
+	// }
+	// changeSlide(0);
+
 
   $(".promo-item-content p").addClass("hidden-2xs");
 });
